88393e430335aa2975944f62f1b3995c5ebe18f9
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-19 15:47:05 -0500

    two_sum

---
+++ two_sum/two_sum.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+import Foundation
+
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//You can return the answer in any order.
+
+//Example 1:
+//
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+//Example 2:
+//
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+//Example 3:
+//
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int]{
+    for i in 0..<nums.count{ // first int index
+        for j in i + 1..<nums.count{ // second int index
+            if nums[i] + nums[j] == target{ // if first int + second int equals target
+
+                return [i,j] // return first and second int
+            }
+        }
+    }
+    return []
+}
+    
+    
+print(twoSum([1,2,3,4,5], 7))
+
+
+    
+
